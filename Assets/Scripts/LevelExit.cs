9e85de05335f88cd283c654b71b42040d850247f
FynsonSOS <yanmuifa@yahoo.com.hk>
2022-05-09 16:23:02 +0800

    Persist

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -23,6 +23,8 @@ public class LevelExit : MonoBehaviour
         {
             nextSceneIndex = 0;
         }
+        FindObjectOfType<ScenePersist>().ResetScenePersist();
         SceneManager.LoadScene(nextSceneIndex);
+
     }
 }
